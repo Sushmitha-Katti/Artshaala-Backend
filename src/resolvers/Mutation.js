9a37753525a462c2e5e2491a206f1af4a3554bdf
Sushmitha M Katti <sushmitha.katti.19@gmail.com>
2019-04-26 01:11:26 +0530

    add create newsletter mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -7,6 +7,22 @@ const { randomBytes } = require("crypto");
 //const stripe = require("../stripe");
 
 const Mutations = {
+  //----------------------------------Newsletter-----------------------------------------
+  async createNewsletter(parent, args, cts, info){
+    const newsletter = await ctx.db.mutation.createNewsletter(
+      {
+        data:{
+          ...args,
+        }
+      },
+      info
+    );
+    return newsletter;
+
+  },
+
+
+
   //--------------------------------- for creating new item-------------------------------
   async createItem(parent, args, ctx, info) {
     console.log(ctx.request.userId);
@@ -205,6 +221,7 @@ const Mutations = {
       const orderItem = {
         ...cartItem.item,
         quantity: cartItem.quantity,
+        itemid:cartItem.item.id,
         user: { connect: { id: userId } }
       };
       delete orderItem.id;
@@ -366,5 +383,7 @@ const Mutations = {
     return comment;
   },
 };
+
+
 
 module.exports = Mutations;
